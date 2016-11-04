df06e255edff55506cbb64664ae621bc95a5e8b1
JIGAR DESAI <jigardesai@JIGARs-MacBook-Pro.local>
2016-11-04 09:34:08 +0530

    changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var kafka = require('kafka-node');
+//var kafka = require('kafka-node-slim');
 var zookeeper = require('node-zookeeper-client');
 
 var Producer = kafka.Producer;
@@ -6,21 +7,82 @@ var KeyedMessage = kafka.KeyedMessage;
 var Client = kafka.Client;
 var producer = null ;
 var argv = require('optimist').argv;
-var topic = argv.topic || 'topic1';
+var topic = 'HIGH_PROCESSING_QUEUE';
 var p = argv.p || 0;
 var a = argv.a || 0;
 
 
 
-function initProducerAndSendMessage(kafkaIp,kafkaPort,zookeeperBrokerList,kafkaTopic,sendMessage,eventraiser,eventname){
+function initProducerAndSendListMessages(kafkaIp,kafkaPort,kafkaTopic,sendMessage,eventraiser,eventname){
    var kafkaServer = kafkaIp + ":" + kafkaPort;
    var kafkaClientId = 'kafka-client-r3';
    var message = sendMessage;
    var messages = [{ topic: kafkaTopic, partition: 0, messages: [message], attributes: 0 }];
   // var keyedMessage = new KeyedMessage('keyed', 'a keyed message');
+  console.log("initProducerAndSendMessageJig");
+
+   client = new Client(kafkaServer,kafkaClientId,undefined,undefined); 
+   producer = new Producer(client,{ requireAcks: 1 });
+
+   producer.on('ready', function () {
+    console.log("onready in");
+
+    var message = 'a message first';
+    
+   
+    producer.send(messages, function (err, result) {
+
+        console.log("send");
+            
+            if(err){
+                console.log(err);
+                try{
+               // eventraiser.emit(eventname, err);
+                } catch (err){
+                    console.log(err);
+                
+                }
+                return false;
+            }else{
+                console.log(result);
+                try{
+               // eventraiser.emit(eventname, result);
+                } catch (err){
+                    console.log(err);
+                
+                }
+
+                return true;
+            }
+        });
+
+     console.log("onready out");   
+    });
+
+    producer.on('error', function (err) {
+        console.log('error', err);
+        try{
+             eventraiser.emit(eventname, err);
+            } catch (err){
+                console.log(err);
+            
+            }
+    });
+
+
+}
+
+exports.initProducerAndSendListMessages = initProducerAndSendListMessages;
+
+function initProducerAndSendMessage(kafkaIp,kafkaPort,kafkaTopic,sendMessage,eventraiser,eventname){
+   var kafkaServer = kafkaIp + ":" + kafkaPort;
+   var kafkaClientId = 'kafka-client-r3';
+   var message = sendMessage;
+   var messages = [{ topic: kafkaTopic, partition: 0, messages:  JSON.stringify(sendMessage), attributes: 0 }];
+  // var keyedMessage = new KeyedMessage('keyed', 'a keyed message');
   console.log("initProducerAndSendMessage");
 
-   client = new Client(kafkaServer,kafkaClientId,zookeeperBrokerList); 
+   client = new Client(kafkaServer,kafkaClientId,undefined,undefined); 
    producer = new Producer(client,{ requireAcks: 1 });
 
    producer.on('ready', function () {
--- test.js
+++ test.js
@@ -2,4 +2,6 @@ var kafka = require('./index.js');
 var events = require('events');
 var emitter       = new events.EventEmitter();
 
+var zookeeperList = ["23.227.167.180:9092,104.251.214.144:9092,63.142.253.79:9092"];
+
 kafka.initProducerAndSendMessage("23.227.167.180","2181","HIGH_PROCESSING_QUEUE","kafkaMessage",emitter,"kafkasentmessage");
